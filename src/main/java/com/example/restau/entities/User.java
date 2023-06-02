ad843d49a70ddbccea9700151191fc52ab11330b
maalfiwissal <maalfiwissal@gmail.com>
2023-06-02 23:34:25 +0100

    build

--- src/main/java/com/example/restau/entities/User.java
+++ src/main/java/com/example/restau/entities/User.java
@@ -9,10 +9,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 
 
 @Entity
+@Table(name = "\"user\"")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
